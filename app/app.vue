f3bb5db229b874b3529cea24e1281e3ab89f1889
Nándor Dudás <nandor.dudas@gmail.com>
2025-02-02 18:46:31 +0100

    chore: setup
    Signed-off-by: Nándor Dudás <nandor.dudas@gmail.com>

---
+++ app/app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { en } from '@nuxt/ui/locale'
+
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+    dir: 'ltr',
+  },
+})
+</script>
+
+<template>
+  <NuxtRouteAnnouncer />
+  <NuxtLoadingIndicator />
+
+  <UApp :locale="en">
+    <NuxtLayout>
+      <NuxtPage :page-key="route => route.fullPath" />
+    </NuxtLayout>
+  </UApp>
+</template>
